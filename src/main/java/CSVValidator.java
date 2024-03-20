69f7df1d900a35fcbe5e8c9b388df20dc2bc40f7
artur <zieloneimperium230@o2.pl>
2024-03-20 20:44:39 +0100

    init project

---
+++ src/main/java/CSVValidator.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration()
+public class CSVValidator {
+    public static void main(String[] args) {
+        SpringApplication.run(CSVValidator.class);
+    }
+}
