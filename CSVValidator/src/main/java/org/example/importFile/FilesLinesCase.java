1b2e63708c41ff3c2fa3109666dce128e22eb9f8
artur <zieloneimperium230@o2.pl>
2025-06-04 12:53:58 +0200

    optimization

--- CSVValidator/src/main/java/org/example/importFile/FilesLinesCase.java
+++ CSVValidator/src/main/java/org/example/importFile/FilesLinesCase.java
@@ -36,26 +36,27 @@ public class FilesLinesCase implements CSVImport {
                 if (isLogging && lineNumber[0] % 1_000_000 == 0) {
                     System.out.println(lineNumber[0]);
                 }
-                if (line.startsWith(CSV_COMMENT)) {
+                if (line.charAt(0) == CSV_COMMENT_CHAR) {
                     lineNumber[0]++;
                     return;
                 }
                 String[] tokens = line.split(CSV_DELIMITER);
-                if (tokens[0].equals(M) && tokens.length == NUMBER_OF_MASTER_DATA_FIELDS) {
+                String firstToken = tokens[0];
+                if (firstToken.equals(M) && tokens.length == NUMBER_OF_MASTER_DATA_FIELDS) {
                     ValidationManager validation = new ValidationManager();
                     ValidationContainer validationContainer = new ValidationContainer(tokens, new MasterData(),
                             masterKeys, actualDataUniques, new LinkedList<>(), lineNumber[0]);
-                    if (validation.isValid(validationContainer, tokens)) {
+                    if (validation.isValid(validationContainer, firstToken)) {
                         resultContainer.masterData().add(validationContainer.data());
                     } else {
                         resultContainer.errors().add(VALIDATION_ERROR + line);
                         resultContainer.errors().addAll(validationContainer.errors());
                     }
-                } else if (tokens[0].equals(A) && tokens.length == NUMBER_OF_ACTUAL_DATA_FIELDS) {
+                } else if (firstToken.equals(A) && tokens.length == NUMBER_OF_ACTUAL_DATA_FIELDS) {
                     ValidationManager validation = new ValidationManager();
                     ValidationContainer validationContainer = new ValidationContainer(tokens, new ActualData(),
                             masterKeys, actualDataUniques, new LinkedList<>(), lineNumber[0]);
-                    if (validation.isValid(validationContainer, tokens)) {
+                    if (validation.isValid(validationContainer, firstToken)) {
                         resultContainer.actualData().add(validationContainer.data());
                     } else {
                         resultContainer.errors().add(VALIDATION_ERROR + line);
